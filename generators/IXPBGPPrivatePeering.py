c43d9bb1d2cad37420ba21a79266a543d9d7bcd4
wvandeun <wim@opsmill.com>
2024-07-25 14:15:14 +0200

    move utils content into generator file

--- generators/IXPBGPPrivatePeering.py
+++ generators/IXPBGPPrivatePeering.py
@@ -1,8 +1,39 @@
+from typing import Union
+
+from infrahub_sdk import InfrahubClient, InfrahubNode
 from infrahub_sdk.exceptions import NodeNotFoundError
 from infrahub_sdk.generator import InfrahubGenerator
+from infrahub_sdk.node import RelatedNode, Attribute
+
+# from utils import inherit_attribute_from_hierarchy, InheritanceException
+
+async def inherit_attribute_from_hierarchy(
+    client: InfrahubClient, node: InfrahubNode, attribute: str
+) -> Union[int, str, bool, InfrahubNode]:
+    if hasattr(node, attribute):
+        attr = getattr(node, attribute)
+        if isinstance(attr, RelatedNode) and attr.schema.cardinality == "one":
+            await attr.fetch()
+            return attr.peer
+        elif isinstance(attr, RelatedNode) and attr.schema.cardinality == "many":
+            raise InheritanceException(f"Relationships of cardinality many are not supported!")
+        elif isinstance(attr, Attribute):
+            return attr.value
+
+    if not hasattr(node, "parent"):
+        raise InheritanceException(f"Could not resolve {attribute} for {node.typename}")
+
+    await node.parent.fetch()
+
+    if not node.parent.peer:
+        raise InheritanceException(f"Could not resolve {attribute} for {node.typename}")
+
+    return await inherit_attribute_from_hierarchy(client, node.parent.peer, attribute)
 
 
-from utils import inherit_attribute_from_hierarchy, InheritanceException
+class InheritanceException(Exception):
+    pass
+
 
 class Generator(InfrahubGenerator):
     async def generate(self, data: dict) -> None:
